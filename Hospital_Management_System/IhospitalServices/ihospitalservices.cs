cb3e8756e5532124e867a2ae9a306e4e8d353082
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-19 14:37:17 +0530

    ihospitalservices.cs

--- Hospital_Management_System/IhospitalServices/ihospitalservices.cs
+++ Hospital_Management_System/IhospitalServices/ihospitalservices.cs
@@ -8,5 +8,10 @@ namespace Hospital_Management_System.IhospitalServices
 {
     public interface ihospitalservices
     {
+        void add(PatientDetails patient);
+        List<PatientDetails> getPatient();
+        PatientDetails getById(int id);
+        void Update(PatientDetails updatedPatient);
+        void Delete(int id);
     }
 }
